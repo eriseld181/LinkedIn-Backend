b6bfa3978e92552a0734149b7a4a542eef3e6bf3
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2021-01-04 22:28:28 +0100

    import error handlers and connect server with database

--- src/server.js
+++ src/server.js
@@ -1,4 +1,39 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = process.env.PORT;
 const server = express();
+const {
+  badRequestHandler,
+  unauthorizedRequestHandler,
+  forbiddenRequestHandler,
+  notFoundRequestHandler,
+  internalServerErrorRequestHandler,
+  badGatewayRequestHandler,
+  serviceUnavailableRequestHandler,
+  gatewayTimeoutRequestHandler,
+} = require("./errorHandlers");
+
 server.use(express.json());
-console.log("hello");
+server.use(cors());
+server.use(badRequestHandler);
+server.use(unauthorizedRequestHandler);
+server.use(forbiddenRequestHandler);
+server.use(notFoundRequestHandler);
+server.use(internalServerErrorRequestHandler);
+server.use(badGatewayRequestHandler);
+server.use(serviceUnavailableRequestHandler);
+server.use(gatewayTimeoutRequestHandler);
+
+//connecting server with mongodb
+mongoose
+  .connect(process.env.MongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    server.listen(port || 3009, () => {
+      console.log("Server is running on port", port || 3009);
+    })
+  )
+  .catch((err) => console.log(err));
