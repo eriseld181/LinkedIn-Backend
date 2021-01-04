f95772c4b9339fd639d5640c0b9b4cef75408f71
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2021-01-04 22:45:37 +0100

    update schema

--- src/routes/experiences/expSchema.js
+++ src/routes/experiences/expSchema.js
@@ -1,39 +1,44 @@
 const { Schema } = require("mongoose");
 const mongoose = require("mongooose");
 
-const exSchema = new Schema({
-  role: {
-    type: String,
-    required: true,
-  },
-  company: {
-    type: String,
-    required: true,
-  },
-  startDate: {
-    type: String,
-    required: true,
-  },
-  endDate: {
-    type: String,
-    required: true,
-  },
+const exSchema = new Schema(
+  {
+    role: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+      required: true,
+    },
 
-  description: {
-    type: String,
-    required: true,
-  },
-  area: {
-    type: String,
-    required: true,
-  },
+    description: {
+      type: String,
+      required: true,
+    },
+    area: {
+      type: String,
+      required: true,
+    },
 
-  username: {
-    type: String,
+    username: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
   },
-  image: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Experiences", exSchema);
