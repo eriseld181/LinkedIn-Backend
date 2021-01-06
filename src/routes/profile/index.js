5a8acc67a45740510ef9817f48ccc0c4bdb4e941
alekszone <50597224+alekszone@users.noreply.github.com>
2021-01-06 22:38:51 +0100

    profile

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -12,17 +12,35 @@ const {
 
 userRouter.post("/login", async (req, res, next) => {
   try {
-  } catch (err) {
-    next(err);
+    const { email, password } = req.body;
+    const user = await userSchema.findByCredentials(email, password);
+    const token = await authenticate(user);
+    res.cookie("accessToken", token.token, {
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+    });
+    res.cookie("refreshToken", token.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      path: "/refreshToken",
+      sameSite: "none",
+    });
+
+    res.send("ok");
+  } catch (error) {
+    next(error);
   }
 });
 
 userRouter.post("/register", async (req, res, next) => {
   try {
-    const newUser = new UserModel(req.body);
+    const newUser = new userSchema(req.body);
     const { _id } = await newUser.save();
     res.status(201).send(`you have been login`);
   } catch (err) {
     next(err);
   }
 });
+
+module.exports = userRouter;
